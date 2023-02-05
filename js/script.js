41d3d95822fe1e0781514e601ee1cb9289adc0d1
Vseslav <Vseslav2244@gmail.com>
2023-02-05 17:41:33 +0300

    alerts changed, php returns page

--- js/script.js
+++ js/script.js
@@ -3,11 +3,7 @@ function isNumeric(n) {
 }
 
 function checkX() {
-    return $(".input-X:checked").length > 0;
-}
-
-function getX(){
-    return $(".input-X:checked").val();
+    return $(".input-X:checked").length > 0 && isNumeric($(".input-X:checked").val());
 }
 
 function checkY() {
@@ -33,16 +29,6 @@ function checkR() {
     return counter === 1;
 }
 
-function getR(){
-    let result = 0;
-    for (let i = 1; i <= 5; i++) {
-        if ($('#r-checkbox' + i).is(":checked")) {
-            result = $('#r-checkbox' + i).val();
-        }
-    }
-    return result;
-}
-
 function validateVar() {
     let response = '';
     if (!checkX()) {
@@ -57,65 +43,20 @@ function validateVar() {
 
     let result = checkY() && checkR() && checkX();
     if (!result){
-        alert(response);
+        //alert(response);
     }
     return result;
 }
 
-let count = 1
-const form = document.querySelector('#js-form');
-form.onsubmit = function (event) {
-    event.preventDefault();
-    if (!validateVar()) {
-        return;
-    }
+function setRequire(elClass) {
+    let el = document.getElementsByClassName(elClass);
 
-    let xhr = new XMLHttpRequest();
-    xhr.onload = function (){
-        let arrayOfValues = xhr.responseText;
-        arrayOfValues = arrayOfValues.split(' ');
-        //alert(arrayOfValues);
-        createNewRow(arrayOfValues[0], arrayOfValues[1], arrayOfValues[2], arrayOfValues[3], arrayOfValues[4], arrayOfValues[5])
-    }
-    xhr.open('GET', 'php/main.php?' + 'countValues=' + count + '&x=' + getX() + '&y=' + getY() + '&r=' + getR());
-    xhr.send();
-}
-
-function createNewRow(x, y, r, date, duration, result) {
-    let tableRef = document.getElementById("table");
-    let row = document.createElement("tr");
-    row.setAttribute('class', 'infoOfRequest table1');
-    row.setAttribute("align", "center");
-    tableRef.append(row);
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    let td4 = document.createElement('td');
-    td1.innerHTML = count;
-    count += 1;
-
-    td2.innerHTML = x + " " +y + " " + r;
-    td3.innerHTML = date;
-    td4.innerHTML = result + " " + duration + " секунд";
-    row.append(td1);
-    row.append(td2);
-    row.append(td3);
-    row.append(td4);
-}
-
-
-window.onload = function () {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'php/data.php?', false);
-    xhr.send();
-
-    if (xhr.status !== 200) {
-        alert(xhr.status + ': ' + xhr.statusText);
-    } else {
-        let arrayOfValues = xhr.responseText;
-        arrayOfValues = arrayOfValues.split(' ');
-        for (let i = 0; i < arrayOfValues.length - 1; i += 6) {
-            createNewRow(arrayOfValues[i], arrayOfValues[i + 1], arrayOfValues[i + 2], arrayOfValues[i + 3],arrayOfValues[i + 4],arrayOfValues[i + 5])
+    let atLeastOneChecked = false; //at least one cb is checked
+    for (let i = 0; i < el.length; i++) {
+        if (el[i].checked === true) {
+            atLeastOneChecked = true;
         }
     }
+
+    el[0].required = atLeastOneChecked !== true;
 }
--- php/main.php
+++ php/main.php
@@ -1,9 +1,97 @@
 <?php
 
-function checkArea($x, $y, $r) : bool
+main();
+
+function main(): void
 {
+    session_start();
+    $x = $_GET['x'];
+    $y = $_GET['y'];
+    str_replace(',', '.', $y);
+    $r = $_GET['r'];
+    $startTime = microtime(true);
+    $correct = checkValues($x, $y, $r);
+    $result = checkArea($x, $y, $r, $correct);
+    $curr = array(
+        "x" => $x,
+        "y" => $y,
+        "r" => $r,
+        "is_hit" => $result
+    );
+
+    if (isset($_SESSION['saves'])) {
+        $saves = unserialize($_SESSION['saves']);
+    } else {
+        $saves = array();
+    }
+    $saves[] = $curr;
+    $_SESSION['saves'] = serialize($saves);
+
+        echo '
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Результаты</title>
+            
+                <!-- Import styles-->
+                <link rel="stylesheet" href="../css/style.css">
+            </head>
+            <body>
+                <header class="header">
+                    Демидович Всеслав, P32091
+                </header>';
+
+        echo "<div class='grid-container'>
+                <div class='header'>X</div>
+                <div class='header'>Y</div>
+                <div class='header'>R</div>
+                <div class='header'>Результат</div>
+    
+    ";
+
+        foreach ($saves as &$element) {
+            echo "
+            <div>" . $element["x"] . "</div>
+            <div>" . $element["y"] . "</div>
+            <div>" . $element["r"] . "</div>
+            <div>" . ($element["is_hit"] ? "да" : "нет") . "</div>";
+        }
+        echo "</div>";
+
+        $date = date("H:i:s");
+        $duration = round(microtime(true) - $startTime, 5);
+
+        echo "Текущая дата: " . $date . "<br>";
+        echo "Время работы скрипта: " . $duration;
+
+        echo '
+            <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+            <script src="../js/script.js"></script>
+        </body>
+    </html>
+    ';
+
+// Очистить данные сессии
+    $reset = $_GET['reset'] ?? 'no';
+    if ($reset !== 'no') {
+        $_SESSION['saves'] = serialize(array());
+    }
+}
+
+function checkValues($x, $y, $r) : bool{
+    return isset($x) && isset($y) && isset($r)
+        && is_numeric($x) && is_numeric($y) && is_numeric($r)
+        && -2 <= $x and $x <= 2 and -5 <= $y and $y <= 3 and 1 <= $r and $r <= 3;
+}
+
+function checkArea($x, $y, $r, $correct) : bool
+{
+    if (!$correct)
+        return false;
+
     if ($x <= 0 && $y <= 0){
-        if ($x >= -$r/2 && $y >= -$r)
+        if ($x >= -$r / 2 && $y >= -$r)
             return true;
     }
     if ($x <= 0 && $y >= 0){
@@ -15,24 +103,4 @@ function checkArea($x, $y, $r) : bool
             return true;
     }
     return false;
-}
-
-main();
-
-function main(): void
-{
-    session_start();
-    $startTime = microtime(true);
-    $countValues = $_GET['countValues'];
-    $x = $_GET['x'];
-    $y = $_GET['y'];
-    $r = $_GET['r'];
-    $result = checkArea($x, $y, $r) ? 'true' : 'false';
-    $date = date("H:i:s");
-    $duration = round( microtime(true) - $startTime,5);
-
-    $arrayOfValues = array($x, $y, $r, $date, $duration, $result);
-    $_SESSION['value' . $countValues] = $arrayOfValues;
-
-    echo $x, ' ', $y, ' ' , $r, ' ',  $date, ' ', $duration, ' ', $result, ' ';
 }
